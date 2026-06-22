4720a3c2c3914be70b33a9dc529257cb474cf221
Siqi Yin <siqi.yin@nhs.net>
2026-06-22 11:52:29 +0100

    Modify BASE_PATH and OUT_PATH for local environment
    Updated file paths for CSV merging functionality.

--- EBH-5123/CSV_merge.py
+++ EBH-5123/CSV_merge.py
@@ -5,8 +5,11 @@ import glob
 import pandas as pd
 
 #Path
-BASE_PATH = r"\\Clingen\cg\Genetics\WGS\WGS_Excel\Sendout CSVs"
+#BASE_PATH = r"\\Clingen\cg\Genetics\WGS\WGS_Excel\Sendout CSVs"
 #OUT_PATH = r"\\appdata\clingencg\Regional Genetics Laboratories\Bioinformatics\temp_siqi\29052026_WGS_CSV_merge"
+
+BASE_PATH = r"/Users/YIN/Library/CloudStorage/OneDrive-NHS/Clinical_Bioinformatics_Genomics_STP/Clinical_Bioinformatician/Help_desk/29052026_WGS_CSV_merge.py/Sendout_CSV"
+OUT_PATH = r"/Users/YIN/Library/CloudStorage/OneDrive-NHS/Clinical_Bioinformatics_Genomics_STP/Clinical_Bioinformatician/Help_desk/29052026_WGS_CSV_merge.py/merged_output"
 
 def merge_genetics_csv(base_path,output_path):
     all_dfs = []
